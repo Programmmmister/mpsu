0ffa2c3ac5de8ee82634e542d720aa3a5ebca165
Programmmmister <tomodashi.2k@gmail.com>
2021-08-30 01:28:20 +0300

    getsize

--- src/DoubleLinkedList/DoubleLinkedList.java
+++ src/DoubleLinkedList/DoubleLinkedList.java
@@ -17,5 +17,25 @@ public class DoubleLinkedList<T> {
         end = start;
     }
 
+    // GET INFO
+
+    public int getSize() {
+
+        // returns number of nodes
+
+        Node<T> iterator = start;
+        int size = 1;
+
+        if (start == null) {
+            return 0;
+        }
+
+        while (iterator.getNext() != null) {
+            size++;
+            iterator = iterator.getNext();
+        }
+
+        return size;
+    }
 
 }
