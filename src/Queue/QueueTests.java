9409c29a50063d4d081c68a9d48c4638b8fdc6e4
Programmmmister <tomodashi.2k@gmail.com>
2021-08-30 15:19:25 +0300

    peek_getsize_tests

--- src/Queue/QueueTests.java
+++ src/Queue/QueueTests.java
@@ -1,5 +1,6 @@
 package Queue;
 
+import Stack.Stack;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,6 +27,45 @@ public class QueueTests {
 
         expected = 5;
         actual = queue.peek();
+        assertEquals(expected, actual);
+    }
+
+    // PEEK TESTS
+
+    @Test
+    public void peek() {
+        Queue<Integer> queue = new Queue<Integer>();
+
+        queue.enqueue(1);
+        queue.enqueue(1);
+        queue.enqueue(3);
+
+        int expected = 3;
+        int actual = queue.peek();
+        assertEquals(expected, actual);
+    }
+
+    // GETSIZE TESTS
+
+    @Test
+    public void getSize() {
+        Queue<Integer> queue = new Queue<Integer>();
+
+        queue.enqueue(1);
+        queue.enqueue(1);
+        queue.enqueue(1);
+
+        int expected = 3;
+        int actual = queue.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getSize_empty() {
+        Queue<Integer> stack = new Queue<Integer>();
+
+        int expected = 1;
+        int actual = stack.getSize();
         assertEquals(expected, actual);
     }
 }
