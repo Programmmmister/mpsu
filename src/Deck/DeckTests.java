d91f0d70539524d092817688ce95ad13c79401ec
Programmmmister <tomodashi.2k@gmail.com>
2021-09-03 22:26:26 +0300

    push_fronttests

--- src/Deck/DeckTests.java
+++ src/Deck/DeckTests.java
@@ -14,4 +14,36 @@ public class DeckTests {
         assertEquals(expected, actual);
     }
 
+    // pushFront tests
+
+    @Test
+    public void pushFront_sizeTest() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.pushFront(1);
+        deck.pushFront(1);
+        deck.pushFront(1);
+        deck.pushFront(1);
+        deck.pushFront(1);
+
+        int expected = 5;
+        int actual = deck.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void pushFront_dataTest() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.pushFront(1);
+        deck.pushFront(2);
+        deck.pushFront(3);
+        deck.pushFront(4);
+        deck.pushFront(5);
+
+        int expected = 5;
+        int actual = deck.peekFront();
+        assertEquals(expected, actual);
+    }
+
 }
