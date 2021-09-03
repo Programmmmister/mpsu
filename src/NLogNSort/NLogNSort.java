206ce88aaa771c87ddccb405f8957d0ef4795337
Programmmmister <tomodashi.2k@gmail.com>
2021-09-03 23:11:22 +0300

    nlongnsort moving to another file + dividing

--- src/NLogNSort/NLogNSort.java
+++ src/NLogNSort/NLogNSort.java
@@ -1,4 +1,48 @@
 package NLogNSort;
 
 public class NLogNSort {
+    public void sort(int[] data)
+    {
+        quicksort(data, 0, data.length -1);
+    }
+
+    private static void quicksort(int[] array, int left, int right) {
+        if (left >= right) {
+            // entry data validation
+            return;
+        }
+
+        int divider = divide(array, left, right, array[right-1]);
+        quicksort(array, left, divider - 1);
+        quicksort(array, divider, right);
+    }
+
+    private static int divide(int[] array, int left, int right, int pivot) {
+        while (left <= right) {
+
+            while (array[left] < pivot) {
+
+                left++;
+            }
+
+            while (array[right] > pivot) {
+
+                right--;
+            }
+
+            if (left <= right) {
+
+                swap(array, left, right);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+
+    private static void swap(int[] array, int indexOne, int indexTwo) {
+        int temp = array[indexOne];
+        array[indexOne] = array[indexTwo];
+        array[indexTwo] = temp;
+    }
 }
--- src/NLogNSort/NLogNSortTest.java
+++ src/NLogNSort/NLogNSortTest.java
@@ -2,29 +2,5 @@ package NLogNSort;
 
 public class NLogNSortTest {
 
-    public void sort(int[] data)
-    {
-        quicksort(data, 0, data.length -1);
-    }
 
-    private static void quicksort(int[] array, int left, int right) {
-        if (left >= right) {
-            // entry data validation
-            return;
-        }
-
-        int divider = divide(array, left, right, array[right-1]);
-        quicksort(array, left, divider - 1);
-        quicksort(array, divider, right);
-    }
-
-    private static int divide(int[] array, int left, int right, int pivot) {
-        //
-    }
-
-    private static void swap(int[] array, int indexOne, int indexTwo) {
-        int temp = array[indexOne];
-        array[indexOne] = array[indexTwo];
-        array[indexTwo] = temp;
-    }
 }
