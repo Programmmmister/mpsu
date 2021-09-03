dc18b75e8d6fb3f2aa3c4dcbb171cd91a3bebe6a
Programmmmister <tomodashi.2k@gmail.com>
2021-09-03 23:13:26 +0300

    nlongnsort emptyArray_doNothing

--- src/NLogNSort/NLogNSortTest.java
+++ src/NLogNSort/NLogNSortTest.java
@@ -1,6 +1,17 @@
 package NLogNSort;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class NLogNSortTest {
 
+    @Test
+    public void emptyArray_doNothing()
+    {
+        int[] actual = new int[0];
+        int[] expected = new int[0];
+        NLogNSort sort = new NLogNSort();
+        sort.sort(actual);
 
+        assertArrayEquals(expected, actual);
+    }
 }
