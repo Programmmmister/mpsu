052fe3d96809bc5553090095c8d2bbc26f9b4858
Programmmmister <tomodashi.2k@gmail.com>
2021-09-03 23:09:05 +0300

    nlongnsort swap

--- src/NLogNSort/NLogNSortTest.java
+++ src/NLogNSort/NLogNSortTest.java
@@ -8,6 +8,23 @@ public class NLogNSortTest {
     }
 
     private static void quicksort(int[] array, int left, int right) {
+        if (left >= right) {
+            // entry data validation
+            return;
+        }
+
+        int divider = divide(array, left, right, array[right-1]);
+        quicksort(array, left, divider - 1);
+        quicksort(array, divider, right);
+    }
+
+    private static int divide(int[] array, int left, int right, int pivot) {
         //
+    }
+
+    private static void swap(int[] array, int indexOne, int indexTwo) {
+        int temp = array[indexOne];
+        array[indexOne] = array[indexTwo];
+        array[indexTwo] = temp;
     }
 }
