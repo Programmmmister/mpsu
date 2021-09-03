288bfc9dd317b3354fdb2bf66f6bffd1ba0838c5
Programmmmister <tomodashi.2k@gmail.com>
2021-09-03 22:51:39 +0300

    emptyarray_test

--- src/QuadraticSort/QuadraticSortTests.java
+++ src/QuadraticSort/QuadraticSortTests.java
@@ -1,4 +1,16 @@
 package QuadraticSort;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class QuadraticSortTests {
+    @Test
+    public void emptyArray_doNothing()
+    {
+        int[] actual = new int[0];
+        int[] expected = new int[0];
+        QuadraticSort sort = new QuadraticSort();
+        sort.sort(actual);
+
+        assertArrayEquals(expected, actual);
+    }
 }
