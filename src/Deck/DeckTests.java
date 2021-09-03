6adc4b320aa74602bf6ed1dbe34b164b00f7477f
Programmmmister <tomodashi.2k@gmail.com>
2021-09-03 22:32:02 +0300

    popFront_tests

--- src/Deck/DeckTests.java
+++ src/Deck/DeckTests.java
@@ -46,4 +46,52 @@ public class DeckTests {
         assertEquals(expected, actual);
     }
 
+    // popFront tests
+
+    @Test
+    public void popFront_sizeTest() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.pushBack(1);
+        deck.pushBack(2);
+        deck.pushBack(3);
+
+        deck.popFront();
+
+        int expected = 2;
+        int actual = deck.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void popFront_sizeTest_negativeSize() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.pushBack(1);
+        deck.pushBack(2);
+        deck.pushBack(3);
+
+        deck.popFront();
+        deck.popFront();
+        deck.popFront();
+        deck.popFront();
+
+        int expected = 0;
+        int actual = deck.getSize();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void popFront_dataTest() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.pushBack(1);
+        deck.pushBack(2);
+        deck.pushBack(3);
+
+        int expected = 1;
+        int actual = deck.peekFront();
+        assertEquals(expected, actual);
+    }
+
 }
