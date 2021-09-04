2eb1ed455861e0bad480f8c2556f3c96e1eded43
Programmmmister <tomodashi.2k@gmail.com>
2021-09-04 09:13:19 +0300

    BinarySearchTree init

---
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,14 @@
+package BinarySearchTree;
+
+public class BinarySearchTree {
+    Node root;
+
+    public BinarySearchTree() {
+        root = new Node();
+    }
+
+    public BinarySearchTree(String key) {
+        root = new Node(key);
+    }
+
+}
