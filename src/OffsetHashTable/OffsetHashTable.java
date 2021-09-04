756e133b6a906d3b2baa3231c87bee25886f9edc
Programmmmister <tomodashi.2k@gmail.com>
2021-09-05 01:30:46 +0300

    init_again

--- src/OffsetHashTable/OffsetHashTable.java
+++ src/OffsetHashTable/OffsetHashTable.java
@@ -1,4 +1,12 @@
 package OffsetHashTable;
+import DynamicArray.DynamicArray;
 
 public class OffsetHashTable {
+    int TABLE_SIZE;
+    DynamicArray<TableObject> table;
+
+    public OffsetHashTable(int size) {
+        TABLE_SIZE = size;
+        table = new DynamicArray<TableObject>(size);
+    }
 }
