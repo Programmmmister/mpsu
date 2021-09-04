65a3b23dc5cf251575757dde2318acf406e287c8
Programmmmister <tomodashi.2k@gmail.com>
2021-09-04 15:10:29 +0300

    bst tests min_GeneralTest + max_GeneralTest

--- src/BinarySearchTree/BinarySearchTreeTests.java
+++ src/BinarySearchTree/BinarySearchTreeTests.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BinarySearchTreeTests {
 
     @Test
-    public void insertion_GeneralTest_positive() {
+    public void insertion_GeneralTest() {
         BinarySearchTree tree = new BinarySearchTree("abcdefgh");
         tree.insert("abc");
         tree.insert("abcd");
@@ -25,6 +25,46 @@ public class BinarySearchTreeTests {
 
         expected = "abcdefghi";
         actual = tree.getSuccessor(tree.search("abcdefgh")).getKey();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void min_GeneralTest() {
+        BinarySearchTree tree = new BinarySearchTree("abcdefgh");
+        tree.insert("abc");
+        tree.insert("abcd");
+        tree.insert("abcdefghijk");
+        tree.insert("abcdefghi");
+        tree.insert("ab");
+        tree.insert("abcdefghijklmnopqrst");
+        tree.insert("abcdefghijklmnopq");
+        tree.insert("abcde");
+        tree.insert("a");
+        tree.insert("abcdefghijklmnop");
+        tree.insert("abcdefghijklmnopqr");
+
+        String expected = "a";
+        String actual = tree.getMin().getKey();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void max_GeneralTest() {
+        BinarySearchTree tree = new BinarySearchTree("abcdefgh");
+        tree.insert("abc");
+        tree.insert("abcd");
+        tree.insert("abcdefghijk");
+        tree.insert("abcdefghi");
+        tree.insert("ab");
+        tree.insert("abcdefghijklmnopqrst");
+        tree.insert("abcdefghijklmnopq");
+        tree.insert("abcde");
+        tree.insert("a");
+        tree.insert("abcdefghijklmnop");
+        tree.insert("abcdefghijklmnopqr");
+
+        String expected = "abcdefghijklmnopqrst";
+        String actual = tree.getMax().getKey();
         assertEquals(expected, actual);
     }
 }
