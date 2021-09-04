a0473fe3f6ced0e6b89cf90225ee33d7cbd89004
Programmmmister <tomodashi.2k@gmail.com>
2021-09-05 00:22:48 +0300

    init again

--- src/HashTable/HashTable.java
+++ src/HashTable/HashTable.java
@@ -1,4 +1,24 @@
 package HashTable;
+import DoubleLinkedList.DoubleLinkedList;
+import DynamicArray.DynamicArray;
 
 public class HashTable {
+    int size = 5;
+    DynamicArray<DoubleLinkedList<TableObject>> data;
+
+    public HashTable() {
+        data = new DynamicArray<DoubleLinkedList<TableObject>>(size);
+        for (int i = 0; i < size; i++) {
+            data.insert(i, new DoubleLinkedList<TableObject>());
+        }
+    }
+
+    public HashTable(int size) {
+        this.size = size;
+        data = new DynamicArray<DoubleLinkedList<TableObject>>(this.size);
+
+        for (int i = 0; i < this.size; i++) {
+            data.insert(i, new DoubleLinkedList<TableObject>());
+        }
+    }
 }
