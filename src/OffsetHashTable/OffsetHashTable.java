b3f0473d9f1c251d842ad987271d56672bdf814c
Programmmmister <tomodashi.2k@gmail.com>
2021-09-05 07:32:43 +0300

    remove

--- src/OffsetHashTable/OffsetHashTable.java
+++ src/OffsetHashTable/OffsetHashTable.java
@@ -10,8 +10,8 @@ public class OffsetHashTable {
         table = new DynamicArray<TableObject>(size);
     }
 
-    public void add(String data) {
-        int key = data.hashCode() % TABLE_SIZE; // key is the index
+    public void input(String data) {
+        int key = get_ascii(data) % TABLE_SIZE;
         TableObject newObject = new TableObject(data);
 
         while (table.get(key) != null) {
@@ -24,8 +24,32 @@ public class OffsetHashTable {
         table.insert(key, newObject);
     }
 
+    public void remove(String data) {
+        int index = get_ascii(data) % TABLE_SIZE;
+
+        if (table.get(index).data.equals(data)) {
+            table.remove(index);
+        } else {
+            for (int i = index; i < table.getSize() - 1; i++) {
+                if (table.get(i).data.equals(data) && table.get(i) != null) {
+                    table.remove(i);
+                    return;
+                }
+            }
+        }
+    }
+
     public int hash(int key) {
         return key % TABLE_SIZE;
+    }
+
+    public int get_ascii(String data) {
+        int code = 0;
+        for (int i = 0; i < data.length(); i++) {
+            int ascii = data.charAt(i);
+            code += ascii;
+        }
+        return code;
     }
 }
 
