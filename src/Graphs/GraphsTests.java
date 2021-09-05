ff2f96350313eb0e5583ee869e438b6d39038dcb
Programmmmister <tomodashi.2k@gmail.com>
2021-09-05 09:13:19 +0300

    tests_init

--- src/Graphs/GraphsTests.java
+++ src/Graphs/GraphsTests.java
@@ -1,4 +1,32 @@
 package Graphs;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class GraphsTests {
+
+    @Test
+    public void init_test() {
+        Graph graph = new Graph(1);
+    }
+
+    @Test
+    public void add_valueTest() {
+        Graph graph = new Graph(5);
+        graph.add(3, 2);
+
+        int expected = 2;
+        int actual = graph.get(3).get(0).getData();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void add_sizeTest() {
+        Graph graph = new Graph(5);
+        graph.add(3, 2);
+
+        int expected = 1;
+        int actual = graph.get_size();
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
