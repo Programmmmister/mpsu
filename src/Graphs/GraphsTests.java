bc9e7008d3f86fcfeec8dc37f17a2280c303ffee
Programmmmister <tomodashi.2k@gmail.com>
2021-09-05 09:23:44 +0300

    delete tests

--- src/Graphs/GraphsTests.java
+++ src/Graphs/GraphsTests.java
@@ -29,4 +29,27 @@ public class GraphsTests {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void delete_valueTest() {
+        Graph graph = new Graph(5);
+        graph.add(3, 2);
+        graph.add(1, 1);
+        graph.delete(1, 1);
+
+        int expected = 2;
+        int actual = graph.get(3).get(0).getData();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void delete_sizeTest() {
+        Graph graph = new Graph(5);
+        graph.add(3, 2);
+        graph.delete(3, 2);
+
+        int expected = 0;
+        int actual = graph.get_size();
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
